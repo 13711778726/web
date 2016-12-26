cd64e3516635861408ff92735d76e6284d66ec10
linlizhu <515124226@qq.com>
2016-12-26 22:40:20 +0800

    1111

--- Application/Runtime/Cache/Admin/8fc6fe2806f1faf9ddd0954f281b1a56.php
+++ Application/Runtime/Cache/Admin/8fc6fe2806f1faf9ddd0954f281b1a56.php
@@ -3,6 +3,6 @@
 <title>Insert title here</title>
 </head>
 <frameset cols="200,*" frameborder="no" border="0" framespacing="0" style="">
-	<frame src="/admin/index/left" name="leftFrame" noresize="noresize" id="leftFrame"  scrolling="no"/>	
-	<frame src="/admin/index/main" name="mainFrame" id="mainFrame"  scrolling="auto"/>	
+	<frame src="/Admin/Index/left" name="leftFrame" noresize="noresize" id="leftFrame"  scrolling="no"/>	
+	<frame src="/Admin/Index/main" name="mainFrame" id="mainFrame"  scrolling="auto"/>	
 </frameset>
