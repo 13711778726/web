2b3709096e76c5bda09fbea1815314af6aeb9760
linlizhu <515124226@qq.com>
2016-12-27 16:43:30 +0800

    dele

--- Application/Runtime/Cache/Admin/8fc6fe2806f1faf9ddd0954f281b1a56.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?><head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
-<frameset cols="200,*" frameborder="no" border="0" framespacing="0" style="">
-	<frame src="/Admin/Index/left" name="leftFrame" noresize="noresize" id="leftFrame"  scrolling="no"/>	
-	<frame src="/Admin/Index/main" name="mainFrame" id="mainFrame"  scrolling="auto"/>	
-</frameset>
