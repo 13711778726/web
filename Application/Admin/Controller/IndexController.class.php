5deb4b2a6d1cfb91d9498956b209d6568f2edcb0
linlizhu <515124226@qq.com>
2017-01-03 14:26:21 +0800

    menu handle

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -12,6 +12,19 @@ function index(){
         $this->display();
     }
     function left(){
+        $Menu = M('menu');
+        $arr = [];
+        $parents = $Menu->field('id,name,url,parentid')->where(array('parentid'=>0))->select();
+        $childs = $Menu->field('id,name,url,parentid')->where('parentid!=0')->select();
+        foreach ($parents as $key=>$val){
+            $arr[$key] = $val;
+            foreach ($childs as $k=>$v){
+                if($parents[$key]['id']==$v['parentid']){
+                    $arr[$key]['child'][$k] = $childs[$k];
+                }
+            }
+        }
+        $this->assign('arr',$arr);
         $this->display();
     }
     function main(){
