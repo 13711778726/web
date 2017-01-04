c393459bad7bc67ac8983387e43039cbca5cf38a
linlizhu <515124226@qq.com>
2017-01-04 16:07:21 +0800

    close redis

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -1,6 +1,6 @@
 <?php
 namespace Admin\Controller;
-use Think\Cache\Driver\Redis;
+//use Think\Cache\Driver\Redis;
 class IndexController extends CommonController {
 function index(){
         $this->display();
@@ -28,9 +28,9 @@ function index(){
         $this->display();
     }
     function main(){
-	 $redis = new Redis();
-         $redis->set('username','my redis service ok!!');
-         echo $redis->get('username');
+	 //$redis = new Redis();
+         //$redis->set('username','my redis service ok!!');
+         //echo $redis->get('username');
         $this->display();
     }
 }
