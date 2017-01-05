25aeefbddf6845dbb56332ca6e22b37925b52a85
linlizhu <linlizhu@aipai.com>
2017-01-05 16:32:49 +0800

    add comment

---
+++ Application/Admin/Controller/CommentController.class.php
@@ -0,0 +1,59 @@
+<?php
+namespace Admin\Controller;
+
+class CommentController extends CommonController {
+    //模板
+    public function commentlist() {
+        $this->display();
+    }
+    //数据加载
+    public function commentAjax(){
+        $arr = [];
+        $where = ['isdel'=>0];
+        $Comment = M('comment');
+        $count = $Comment->count();
+        $page = I('request.page',1,'int');
+        $status = I('request.status',-1,'int');
+        if($status!=-1){
+            $where['status'] = $status;
+        }
+        $pageSize = I('request.rows',20,'int');
+        $list=$Comment->where($where)->page($page, $pageSize)->select();
+        foreach ($list as $key=>$val){
+            if($val['replytime'] == 0){
+                $list[$key]['replytime'] = '';
+            }else{
+                $list[$key]['replytime'] = date('Y-m-d H:i:s',$val['replytime']);
+            }
+            $list[$key]['addtime'] = date('Y-m-d H:i:s',$val['addtime']);
+        }
+        $arr['total'] = $count;
+        $arr['pageCount'] = ceil($arr['total'] / $pageSize);
+        $arr['rows'] = $list;
+        return $this->ajaxReturn($arr);
+    }
+    //操作数据
+    function edit(){
+        $return = ['status'=>0,'info'=>'','data'=>array()];
+        $oper = I('request.oper','','string'); 
+        $id = I('request.id',0,'int');
+        $status = I('request.status',-1,'int');
+        $Comment = M('comment');
+        if($status == -1){
+            $return['info'] = '参数缺失';
+            return $this->ajaxReturn($return);
+        }
+        if($oper == 'edit'){
+            $res = $Comment->where(array('id'=>$id))->setField('status',$status);
+        }else if($oper == 'del'){
+            $res = $Comment->where(array('id'=>$id))->save(array('isdel'=>1));
+        }
+        if($res){
+            $return['status'] = 1;
+            $return['info'] = '操作成功';
+        }else{
+            $return['info'] = '操作失败';
+        }
+        return $this->ajaxReturn($return);
+    }
+}
